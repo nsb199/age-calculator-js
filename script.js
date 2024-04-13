9623b1d3f96a105c0858fd6a2dbf8180cdacfebf
nsb199 <singhneeraj199199@gmail.com>
2024-04-13 13:06:35 +0530

    added months and days as well

--- script.js
+++ script.js
@@ -3,34 +3,41 @@ const birthdayEl = document.getElementById("birthday");
 const resultEl = document.getElementById("result");
 
 function calculateAge() {
-    const birthdayValue = birthdayEl.value;
-    if (birthdayValue === "") {
-      alert("Please enter your birthday");
-    } else {
-      const age = getAge(birthdayValue);
-      if (age > 130) {
-        resultEl.innerText = "You should probably be dead 🙂";
-      } else {
-        resultEl.innerText = `Your age is ${age} ${age > 1 ? "years" : "year"} old`;
-      }
-    }
+  const birthdayValue = birthdayEl.value;
+  if (birthdayValue === "") {
+    alert("Please enter your birthday");
+  } else {
+    const age = getAge(birthdayValue);
+    resultEl.innerText = age;
   }
+}
+
   
 
-function getAge(birthdayValue) {
-  const currentDate = new Date();
-  const birthdayDate = new Date(birthdayValue);
-  let age = currentDate.getFullYear() - birthdayDate.getFullYear();
-  const month = currentDate.getMonth() - birthdayDate.getMonth();
-
-  if (
-    month < 0 ||
-    (month === 0 && currentDate.getDate() < birthdayDate.getDate())
-  ) {
-    age--;
+  function getAge(birthdayValue) {
+    const currentDate = new Date();
+    const birthdayDate = new Date(birthdayValue);
+  
+    let ageYears = currentDate.getFullYear() - birthdayDate.getFullYear();
+    let ageMonths = currentDate.getMonth() - birthdayDate.getMonth();
+    let ageDays = currentDate.getDate() - birthdayDate.getDate();
+  
+    if (ageDays < 0) {
+      ageMonths--;
+      ageDays += new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
+    }
+    if (ageMonths < 0) {
+      ageYears--;
+      ageMonths += 12;
+    }
+  
+    if (ageYears > 130) {
+      return "You should probably be dead 🙂";
+    }
+  
+    return `${ageYears} years, ${ageMonths} months, and ${ageDays} days`;
   }
-
-  return age;
-}
+  
+  
 
 btnEl.addEventListener("click", calculateAge);
